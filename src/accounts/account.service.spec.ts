3994b7d9b821fdc4139f4991d3659a206adb51e7
Cory Buecker <email@corybuecker.com>
2023-02-10 08:02:46 -0600

    --wip-- [skip ci]

--- src/accounts/account.service.spec.ts
+++ src/accounts/account.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from '../entities/account';
-import { AccountService } from './account.service';
 import { User } from '../entities/user';
+import { AccountService } from './account.service';
 describe('AccountsService', () => {
   let service: AccountService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AccountService],
-      imports: [TypeOrmModule.forFeature([Account,User])],
+      imports: [TypeOrmModule.forFeature([Account, User])],
     }).compile();
 
     service = module.get<AccountService>(AccountService);
--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { connectionOptions } from '../data-source';
 import { User } from '../entities/user';
 import { UserService } from './user.service';
-import { connectionOptions } from '../data-source';
 
 describe('UsersService', () => {
   let service: UserService;
@@ -10,12 +10,14 @@ describe('UsersService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UserService],
-      imports: [TypeOrmModule.forRoot({
-        ...connectionOptions,
-        autoLoadEntities: true,
-        logging: true,
-      }),TypeOrmModule.forFeature([User])],
-      
+      imports: [
+        TypeOrmModule.forRoot({
+          ...connectionOptions,
+          autoLoadEntities: true,
+          logging: true,
+        }),
+        TypeOrmModule.forFeature([User]),
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
